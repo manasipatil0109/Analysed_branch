117f6fc5b30bcf64204adbdee9b0eb5c8a438c0c
AnushayaKumar <88180421+AnushayaKumar@users.noreply.github.com>
2021-09-22 14:56:16 +0530

    Analysed

---
+++ onboarding_step_1.php
@@ -0,0 +1,89 @@
+<?php include ('header.php')?>
+<link rel="stylesheet"href="./css/onboarding_step_1.css">
+<div class="container-1">
+<div class="bread-crumbs_Mytools-recruiter">
+<a href=""class="active-breadcrumb-link">Dashboard ></a>
+<a href=""class="active-breadcrumb-link">My Database ></a>
+<a href=""class="active-breadcrumb-link">Job listings ></a>
+<a href=""class="active-breadcrumb-link">Acturial Assistant</a>
+</div>
+
+<div class="name">Onboarding process<br>
+<span style="font-size:15px; color:#3589db; font-family:Metro;">Create your own onboarding process</span>
+<p>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/>
+</svg>
+Keep in mind that the order in which you create your steps will be the same <br>
+for candidate when their onboarding starts.</p></div>
+<div class="nav">
+<ul><a href="onboarding_step_1.php">Step-1</a></ul>
+<ul><a href="#" style="color:#979797">Step-2</a></ul>
+<ul><a href="#" style="color:#979797"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#3589db" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+</svg></a></ul>
+</div>
+<div class="box">
+<div class="step">
+Step-1<br>
+<span style="color:#3589db">Terms & Conditions (Company Policy)</span><br>
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+ sed diam nonumy eirmod tempor invidunt ut labore et dolore 
+ magna aliquyam erat, sed diam voluptua. At vero eos
+  et accusam et justo duo dolores et</p>
+</div><br>
+<div class="content">
+<label>Name*</label><br>
+<input type="text" placeholder="Company Policy"><br>
+</div>
+<div class="content1">
+<label>Description*</label><br>
+<input type="text" placeholder="Describe the step and what the candidiate has to do"><br><br>
+<label>Extra Information*</label><br>
+<input type="text" placeholder="Extra Additional Information"><br>
+</div>
+<div class="radio">
+<input type="radio"> Show Agree to Terms and Conditions checkbox</div>
+<div class="editor">
+Text editor<br>
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, <br>
+sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.</p>
+</div>
+<div class="text">
+<div class="text1">
+<p>Text Editor options</p>
+</div>
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr
+, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+ magna aliquyam erat, sed diam voluptua. At vero eos et accusam 
+ et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
+ no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+ Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+  sed diam nonumy eirmod tempor invidunt ut labore et 
+  dolore magna aliquyam erat, sed diam voluptua. At 
+  vero eos et accusam et justo duo dolores et ea rebum.
+   Stet clita kasd gubergren, no sea takimata sanctus est
+    Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+     consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+      invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+      At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
+       nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+         rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur 
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+          labore et dolore magna aliquyam erat, sed diam voluptua. 
+          At vero eos et accusam et justo duo dolores et ea rebum.
+           Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, 
+            consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+             invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+      At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd.</p>
+</div>
+<div class="button"><button>Proceed to draft view</button></div>
+
+
+
+</div>
---
+++ tools1.php
@@ -0,0 +1,42 @@
+<?php include ('header.php')?>
+<link rel="stylesheet" href="./css/tools1.css">
+<div class="container-1">
+ <div class="bread-crumbs_Mytools-recruiter">
+            <a href="" class="active-breadcrumb-link">Dashboard</a> >  <a href="" class="active-breadcrumb-link"><u>Tools</u></a>
+        </div>
+        <div class="nav">
+  <l><a href="#">Mydatabase &#10006;</a></l>
+  <l><a href="#">Joblistings &#10006;</a></l>
+  <l><a href="#">Tools &#10006;</a></l>
+  <l><a href="#">Resume &#10006;</a></l>
+  </div>
+  <div class="name">
+  Resume Sorting<br>
+  <span style="font-size:15px; color:#3589db; font-family:Metro;">Lorem ipsum dolor sit amet, consetetur</span>
+  </div>
+  <div class="nav1">
+  <l><a href="tools1.php" style="color:#3589db; text-decoration:underline;">How To Use </a></l>
+  <l><a href="tools2.php">Usage </a></l>
+  <l><a href="tools3.php">Settings </a></l>
+  </div>
+  <div class="book">
+  <div class="book1">
+  How it works<br>
+  <p style="color:black; font: normal normal normal 15px/26px MetroM;">
+  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+   eirmod tempor invidunt ut labore et dolore magna aliquyam erat
+  , sed diam voluptua. At vero eos et accusam et justo.</p>
+  </div>
+  <div class="book1">
+  Use It To Your Advantange<br>
+  <p style="color:black; font: normal normal normal 15px/26px MetroM;">
+  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+   eirmod tempor invidunt ut labore et dolore magna aliquyam erat
+  , sed diam voluptua. At vero eos et accusam et justo.</p>
+  </div>
+  </div>
+
+
+
+
+  </div>
