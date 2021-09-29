918631afd90dce5702007a8320aa807204662c41
Ellakiyas <ellakiyas3590@gmail.com>
2021-09-29 11:41:09 +0530

    Files added

---
+++ recruiter/contact_specific-6.php
@@ -0,0 +1,288 @@
+<?php include('header.php') ?>
+<title>Contact specific</title>
+<link rel="stylesheet" href="./css/contact_specific-6.css">
+
+<body>
+    <div class="container">
+        <div class="small_container">
+            <div class="bread-crumbs_Mytools-recruiter">
+                <a href="/" class="unactive-breadcrumb-link">Dashboard</a> > <a href="" class="unactive-breadcrumb-link">My Database</a> > <a href="" class="unactive-breadcrumb-link">Add Contact</a> > <a href="" class="unactive-breadcrumb-link"> Client List</a> > <a href="" class="active-breadcrumb-link">Justin Soto</a>
+            </div>
+
+            <div class="profile-img">
+                <img src="img/Ellipse -10@2x.png"  alt="">
+                <h1>Justin Soto<p1> #JB13069</p1></h1><br>
+                <h2>Fox Hunt - <span>HR</span></h2><br>
+                <h4><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/></svg> London</h4>
+            </div>
+            <div class="button">
+                <button>Edit client</button>
+            </div>
+
+            <div class="options">
+                <p><a href="contact_specific-1.php">At a glance</a></p>
+                <p><a href="contact_specific-2.php">Appointments</a></p>
+                <p><a href="contact_specific-3.php">Activity</a></p>
+                <p><a href="contact_specific-5.php">Jobs</a></p>
+                <p><a href="contact_specific-4.php">Files</a></p>
+                <p style="color:#3598DB;margin-left:-2px"> <b style="margin-right: 10px;">|</b>Notes</p>
+                
+            </div>
+
+            <div class="status">
+                <div class="status1" >
+                    <p>#JB13069</p>
+                    <span>05-05-2021</span>
+                </div>
+                <div class="status-name" style="margin-top:30px;">
+                    <span style="margin-left:50px;">Justin soto</span>
+                    <p style="color:#3598DB;margin-left:70px">Fox Hunt <span style="color:black">| HR</span></p>
+                    <u style="color:#979797;margin-left:15px">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</u>
+                    <br><br>
+                    <span style="color:#979797; font-size:15px; margin-left:20px;">Status&emsp;&emsp;&emsp;&emsp;Last Contact</span>
+                    <br>
+                </div>
+
+                <div class="displayname"> 
+                    <span>Live</span>&emsp;&emsp;&emsp;&emsp;&emsp;20-05-2020
+                </div>
+                <br><br>
+
+                <div class="fee">
+                    <span>Division&emsp;&emsp;&emsp;&emsp;Internal</span><br>
+                </div>
+
+                <div class="fee-percent">
+                    <span>Other</span>&emsp;&emsp;&emsp;&emsp;&emsp;System verified
+                </div>
+                <br><br>
+
+                <div class="details">
+                    <p style="color:#979797;padding-top: 0px;">Created by</p>
+                    <br>
+                    <p style="color:#3598DB;">Curtis kelly</p>
+                    <p style="color:black;">abc@gmail.com</p>
+                    <p style="color:black;">+91 9876543210</p>
+                    <br><br>
+                    <p style="color:#979797;padding-top: 0px;">Contact info</p>
+                    <br>
+                    <p style="color:#3598DB;">Emailid@gmail.com</p>
+                    <p style="color:black;">+91 9876543210</p>
+                    <p style="color:black;">+91 9876543210</p>
+                    <br><br>
+                    <p style="color:#979797;padding-top: 0px;">Address</p>
+                    <br>
+                    <p style="color:black">Address line 1</p>
+                    <p style="color:black;">  City-12345</p>
+                    <p style="color: black;">State,Country.</p>
+                    <br><br>
+                </div>
+            </div>
+
+            <div class="left-side-container-1">
+                                    <span class="row-recruiter upcoming_interview_top_span">
+                        <div class="search-box">
+                        <details id="dropdown" class="dropdown">
+                            <summary class="dropdown-button">
+                                <span id="selected-item" class="selected-item"> <i class="fa fa-pencil" aria-hidden="true"></i> Author</span>
+                            </summary>
+                            <details-menu class="dropdown-menu">
+                                <div class="dropdown-menu-list">
+                                    <label class="dropdown-menu-item">
+                                        <input type="radio" name="item" value="1">
+                                        <span>1</span>
+                                    </label>
+                                    <label class="dropdown-menu-item">
+                                        <input type="radio" name="item" value="2">
+                                        <span>2</span>
+                                    </label>
+                                    <label class="dropdown-menu-item">
+                                        <input type="radio" name="item" value="3">
+                                        <span>3</span>
+                                    </label>
+                                    <label class="dropdown-menu-item">
+                                        <input type="radio" name="item" value="4">
+                                        <span>4</span>
+                                    </label>
+                                </div>
+                            </details-menu>
+                        </details>
+                        <input class="search-form-input" type="search" name="search" placeholder="Search for appointment">
+                        <button class="searchButton">
+                            <img src="./img/search-icon-blue.png" />
+                        </button>
+                    </div>
+                    <button class="btn" id="myBtn" type="button">Add +</button>
+                </span>
+                <div class="row-flex-jobj justifycontent-flex-end-1">
+                    <p class="sortbyText">Sort by :
+                        <select name="multi_search" id="multi_search" class="sortbySelect">
+                            <option>Recent</option>
+                            <option value="Recent">All</option>
+                            <option value="Old">Old</option>
+                        </select>
+                        <select  id="multi_search" class="search_list">
+                            <option value="" default class="search_list_option">Last Edited</option>
+                            <option value="1" class="search_list_option">1</option>
+                            <option value="2" class="search_list_option">2</option>
+                            <option value="3" class="search_list_option">3</option>
+                        </select>
+                    </p>
+                </div>
+                <div class="row-recruiter">
+                <div class="select-col">
+                        <input type="checkbox" id="c" name="cb">
+                        <label for="c" class="select-col-label"> Select all</label>
+                    </div>
+                    <select  id="multi_search" class="search_list_1">
+                        <option value="" default>Select action</option>
+                        <option value="1"></option>
+                        <option value="2"></option>
+                        <option value="3"></option>
+                    </select>
+                </div>
+
+                <div class="row">
+                <div class="column">
+                        <input type="checkbox" id="c1" class="checkbox">
+                        <label for="c1"></label>
+                    </div>
+                    <div class="column">
+                        <p class="col-1">Last Updated</p>
+                        <span class="col-span-1">05-05-2020</span>
+                    </div>
+                    <div class="column">
+                        <img src="./img/Ellipse -5.png" class="img">
+                    </div>
+                    <div class="column">
+                        <p class="col-2">Uploaded by</p>
+                        <span class="col-span-2">Sasuke Uchiha</span>
+                    </div>
+                    <div class="column">
+                        <p class="col-3">Type</p>
+                        <span class="col-span-3">Resume evaluation</span>
+                    </div>
+                    <div class="column">
+                        <p class="col-4">Time spent</p>
+                        <span class="col-span-4">30min</span>
+                    </div>
+                    <div class="column">
+                        <p class="col-5">Edit <i class="fa fa-pencil" aria-hidden="true"></i></p>
+                    </div>
+                    <div class="column">
+                        <p class="col-6">Delete <i class="fa fa-trash" aria-hidden="true"></i></p>
+                    </div>
+
+                    <div class="column" style="margin-top:80px;margin-left: -838px;">
+                        <p class="col-1">Added on</p>
+                        <span class="col-span-1">05-05-2020</span>
+                    </div>
+                    <div class="column" style="margin-top:-5px;margin-left: 200px;padding-right: 10px;">
+                        <p style="font-size: 16px;">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum.</p>
+                    </div>
+                </div>
+
+
+                <div class="row">
+
+                    <div class="column">
+                        <input type="checkbox" id="c1" class="checkbox">
+                        <label for="c1"></label>
+                    </div>
+                    <div class="column">
+                        <p class="col-1">Last Updated</p>
+                        <span class="col-span-1">05-05-2020</span>
+                    </div>
+                    <div class="column">
+                        <img src="./img/Ellipse -5.png" class="img">
+                    </div>
+                    <div class="column">
+                        <p class="col-2">Uploaded by</p>
+                        <span class="col-span-2">Sasuke Uchiha</span>
+                    </div>
+                    <div class="column">
+                        <p class="col-3">Type</p>
+                        <span class="col-span-3">Resume evaluation</span>
+                    </div>
+                    <div class="column">
+                        <p class="col-4">Time spent</p>
+                        <span class="col-span-4">30min</span>
+                    </div>
+                    <div class="column">
+                        <p class="col-5">Edit <i class="fa fa-pencil" aria-hidden="true"></i></p>
+                    </div>
+                    <div class="column">
+                        <p class="col-6">Delete <i class="fa fa-trash" aria-hidden="true"></i></p>
+                    </div>
+
+                    <div class="column" style="margin-top:80px;margin-left: -838px;">
+                        <p class="col-1">Added on</p>
+                        <span class="col-span-1">05-05-2020</span>
+                    </div>
+                    <div class="column" style="margin-top:-5px;margin-left: 200px;padding-right: 10px;">
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum.</p>
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="column">
+                        <input type="checkbox" id="c1" class="checkbox">
+                        <label for="c1"></label>
+                    </div>
+                    <div class="column">
+                        <p class="col-1">Last Updated</p>
+                        <span class="col-span-1">05-05-2020</span>
+                    </div>
+                    <div class="column">
+                        <img src="./img/Ellipse -5.png" class="img">
+                    </div>
+                    <div class="column">
+                        <p class="col-2">Uploaded by</p>
+                        <span class="col-span-2">Sasuke Uchiha</span>
+                    </div>
+                    <div class="column">
+                        <p class="col-3">Type</p>
+                        <span class="col-span-3">Resume evaluation</span>
+                    </div>
+                    <div class="column">
+                        <p class="col-4">Time spent</p>
+                        <span class="col-span-4">30min</span>
+                    </div>
+                    <div class="column">
+                        <p class="col-5">Edit <i class="fa fa-pencil" aria-hidden="true"></i></p>
+                    </div>
+                    <div class="column">
+                        <p class="col-6">Delete <i class="fa fa-trash" aria-hidden="true"></i></p>
+                    </div>
+
+                    <div class="column" style="margin-top:80px;margin-left: -838px;">
+                        <p class="col-1">Added on</p>
+                        <span class="col-span-1">05-05-2020</span>
+                    </div>
+                    <div class="column" style="margin-top:-5px;margin-left: 200px;padding-right: 10px;">
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum.</p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</body>
+
+<script>
+var items = document.getElementsByName('item');
+var selectedItem = document.getElementById('selected-item');
+var dropdown = document.getElementById('dropdown');
+
+items.forEach(item => {
+  item.addEventListener('change', () => {
+    if (item.checked) {
+      selectedItem.innerHTML = item.value;
+      dropdown.open = false;
+    }
+  });
+});
+</script>
